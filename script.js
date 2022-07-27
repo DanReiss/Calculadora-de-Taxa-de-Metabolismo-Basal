d8aee758e8d1432d7b181963ee22ebcd89429a8f
Danreiss <danreis.ctt@gmail.com>
2022-07-27 11:07:50 -0300

    Melhorias, não finalizado

--- script.js
+++ script.js
@@ -10,29 +10,38 @@ let ext = document.querySelector('#ext')
 
 let result1 = document.querySelector('#span11')
 let result2 = document.querySelector('#span2')
+let h1span11 = document.querySelector('#h1span11')
+let h1span2 = document.querySelector('#h1span2')
+
 
 function calc1(){
     result1.innerHTML = Math.floor(10 * p.value + 6.25 * a.value + 5 * i.value + 5)
     result1.style.display = 'inline';
+    h1span11.style.display = 'block';
 }
 
 function sedexec(){
     result2.innerHTML = Math.floor(1.2 *(10 * p.value + 6.25 * a.value + 5 * i.value + 5))
     result2.style.display = 'inline';
+    h1span2.style.display = 'block';
 }
 function levexec(){
     result2.innerHTML = Math.floor(1.375 *(10 * p.value + 6.25 * a.value + 5 * i.value + 5))
     result2.style.display = 'inline';
+    h1span2.style.display = 'block';
 }
 function modexec(){
     result2.innerHTML = Math.floor(1.55 *(10 * p.value + 6.25 * a.value + 5 * i.value + 5))
     result2.style.display = 'inline';
+    h1span2.style.display = 'block';
 }
 function altexec(){
     result2.innerHTML = Math.floor(1.725 *(10 * p.value + 6.25 * a.value + 5 * i.value + 5))
     result2.style.display = 'inline';
+    h1span2.style.display = 'block';
 }
 function extexec(){
     result2.innerHTML = Math.floor(1.9 *(10 * p.value + 6.25 * a.value + 5 * i.value + 5))
     result2.style.display = 'inline';
+    h1span2.style.display = 'block';
 }
